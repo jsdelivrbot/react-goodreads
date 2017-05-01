119cf37c8efdda5f773bf8261d6e1521b98f1abc
ovhwgr <oliver.vhwgr@gmail.com>
2017-05-01 18:12:49 +0200

    Add books list and connect to redux / add books reducer

---
+++ src/reducers/booksReducer.js
@@ -0,0 +1,14 @@
+export default function() {
+	return [
+		{ title: 'The Hitchhiker\'s Guide to the Galaxy', author: 'Douglas Adams', pages: '193', published: '1979', goodreadsId: '386162', imageUrl: 'https://images.gr-assets.com/books/1388282444l/386162.jpg'},
+		{ title: 'The Restaurant at the End of the Universe', author: 'Douglas Adams', pages: '250', published: '1980', goodreadsId: '8695', imageUrl: 'https://images.gr-assets.com/books/1390234660l/8695.jpg'},
+		{ title: 'Life, the Universe and Everything', author: 'Douglas Adams', pages: '224', published: '1982', goodreadsId: '8694', imageUrl: 'https://images.gr-assets.com/books/1333577589l/8694.jpg'},
+		{ title: 'So Long, and Thanks for All the Fish', author: 'Douglas Adams', pages: '225', published: '1984', goodreadsId: '6091075', imageUrl: 'https://images.gr-assets.com/books/1327873354l/6091075.jpg'},
+		{ title: 'Mostly Harmless', author: 'Douglas Adams', pages: '288', published: '1992', goodreadsId: '569429', imageUrl: 'https://images.gr-assets.com/books/1397933521l/569429.jpg'},
+		{ title: 'Dirk Gently\'s Holistic Detective Agency', author: 'Douglas Adams', pages: '306', published: '1987', goodreadsId: '365', imageUrl: 'https://images.gr-assets.com/books/1404697381l/365.jpg'},
+		{ title: 'The Long Dark Tea-Time of the Soul', author: 'Douglas Adams', pages: '307', published: '1988', goodreadsId: '357', imageUrl: 'https://images.gr-assets.com/books/1388257271l/357.jpg'},
+		{ title: 'The Salmon of Doubt', author: 'Douglas Adams', pages: '298', published: '2002', goodreadsId: '359', imageUrl: 'https://images.gr-assets.com/books/1388273676l/359.jpg'},
+		{ title: 'I, Partridge: We Need to Talk About Alan', author: 'Rob Gibbons, Neil Gibbons, Armando Iannucci, Steve Coogan', pages: '320', published: '2011', goodreadsId: '12544734', imageUrl: 'https://images.gr-assets.com/books/1335992143l/12544734.jpg'},
+		{ title: 'Alan Partridge: Nomad', author: 'Rob Gibbons, Neil Gibbons, Steve Coogan', pages: '320', published: '2016', goodreadsId: '31188362', imageUrl: 'https://images.gr-assets.com/books/1469102435l/31188362.jpg'},
+	]
+}
